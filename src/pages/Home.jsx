84839b8f927d64f32faa9c2140cba863b6b6a3d7
Matthew <mray@zoll.com>
2023-04-26 19:55:52 -0400

    feature: ads component, google adsense config, topic json for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
+import { Ads } from "../components/Ads";
+
+import topics from '../assets/TopicSummarys.json';
 
 import '../styles/Home.scss';
 
@@ -9,51 +12,24 @@ export const Home = () => {
     <div className="home">
       <Header />
       <section className="home__content">
-        <div className="home__topic--blurb">
-          <h3 className="home__topic--title">Best Licensed Lego Set</h3>
-          <div className="home__topic--date">April 22, 2023</div>
-          <p>Marvel, Star Wars, Disney, etc. What is the best licensed lego sets available? We weill dig into the bricks and give you all of the details.</p>
-          <Link className="home__topic--link" to="/post/1">Read More</Link>
-        </div>
-        <div className="home__topic--image">
-
-        </div>
-
-        <div className="home__topic--image">
-
-        </div>
-        <div className="home__topic--blurb">
-          <h3>Lego Unveils New Star Wars Sets</h3>
-          <p>Lego has just announced the release of several new Star Wars sets, including the iconic Millennium Falcon and the fearsome AT-AT walker. These sets are sure to be a hit with both Star Wars and Lego fans, and are perfect for recreating your favorite scenes from the movies.</p>
-          <Link className="home__topic--link" to="/post/1">Read More</Link>
-        </div>
-
-        <div className="home__topic--blurb">
-          <h3>The Top 10 Lego Sets of All Time</h3>
-          <p>What are the best Lego sets of all time? We've compiled a list of our top 10 favorites, from classic sets like the Lego City Police Station to modern masterpieces like the Lego Architecture series. Whether you're looking for a challenging build or just want to relive some childhood memories, these sets are sure to delight.</p>  
-          <Link className="home__topic--link" to="/post/1">Read More</Link>
-        </div>
-        <div className="home__topic--image">
-
-        </div>
-
-        <div className="home__topic--image">
-
-        </div>
-        <div className="home__topic--blurb">
-          <h3>Lego Ideas: A Platform for Fan-Made Creations</h3>
-          <p>Lego Ideas is a platform that allows Lego fans to submit their own creations for consideration as official Lego sets. From a giant Lego dinosaur to a fully functioning Lego piano, the possibilities are endless. In this article, we'll take a closer look at some of the most impressive fan-made creations on Lego Ideas and explore how the platform works.</p>
-          <Link className="home__topic--link" to="/post/1">Read More</Link> 
-        </div>
-
-        <div className="home__topic--blurb">
-          <h3>How to Build a Lego Castle</h3>
-          <p>Building a Lego castle is a fun and rewarding project that's perfect for all ages. In this tutorial, we'll walk you through the steps of building your own Lego castle, from selecting the right bricks to adding the finishing touches. With a little patience and creativity, you can create a masterpiece that will stand the test of time.</p>
-          <Link className="home__topic--link" to="/post/1">Read More</Link>
-        </div>
-        <div className="home__topic--image">
-
-        </div>
+        {topics.map((topic, index) => {
+          return (
+            <React.Fragment key={`${topic.title}-${index}-${topic.date}`}>
+              <div className="home__topic--image" key={topic.title}>
+                <img src={require(`../assets/${topic.image}.png`)} alt={topic.title} />
+              </div>
+              <div className="home__topic--blurb">
+                <h3 className="home__topic--title">{topic.title}</h3>
+                <div className="home__topic--date">{topic.date}</div>
+                <p>{topic.summary}</p>
+                <Link className="home__topic--link" to={topic.url}>Read More</Link>
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </section>
+      <section className="home__ad-content">
+        <Ads dataAdSlot="1234567890" />
       </section>
     </div>
   );
